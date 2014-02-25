ae170e70d0b0f863761e8ead9b1879adb9622921
Hashrocket Workstation <dev@hashrocket.com>
2014-02-25 05:24:43 -0800

    Moved some common code to support methods

--- spec/models/ladder_spec.rb
+++ spec/models/ladder_spec.rb
@@ -4,16 +4,14 @@ describe Ladder do
   let(:redis) { Redis.current }
   before(:each) do
     redis.flushall
+    30.times do
+      build_ladder_player(region: 'test')
+    end
   end
 
   describe '.find_players_by_rank' do
     it 'returns a paginated list of summoners' do
       ladder = Ladder.new('test')
-      redis.pipelined do
-        30.times do |number|
-          redis.zadd('rank_test', number, "#{number}_test")
-        end
-      end
       expect(ladder.find_players_by_rank(1).count).to eq 25
       expect(ladder.find_players_by_rank(2).count).to eq 5
     end
@@ -22,10 +20,6 @@ describe Ladder do
   describe '.combine_players_with_rank' do
     it 'returns a list of players and ranks' do
       ladder = Ladder.new('test')
-      10.times do |number|
-        create(:player, summoner_id: number, region: 'test')
-        redis.zadd('rank_test', number, "#{number}_test")
-      end
       first_player = ladder.combine_players_with_rank(Player.all).first
       expect(first_player.rank).to eq 0
     end
--- spec/support/ladder.rb
+++ spec/support/ladder.rb
@@ -1,10 +1,26 @@
 include ApplicationHelper
 
-def build_ladder(opts)
-  count = opts[:count]
-  region = opts[:region]
+def build_ladder_player(opts)
+  @region = opts[:region]
+  @summoner_id, @points = randomize_player_values
+  create_new_player
+  create_ranking
+end
 
-  count.times do |n|
-    player = build(:player, summoner_id: n, region: region)
+def randomize_player_values
+  r = Random.new
+  [r.rand(1...1000000), r.rand(1...1000)]
+end
+
+def create_new_player
+  player = build(:player, summoner_id: @summoner_id, region: @region)
+  player.build_player_league(tier: 'Test', league_points: @points)
+  player.save
+end
+
+def create_ranking
+  redis.pipelined do
+    redis.zadd("rank_#{@region}", @points, "#{@summoner_id}_#{@region}")
+    redis.zadd("rank_all", @points, "#{@summoner_id}_#{@region}")
   end
 end
