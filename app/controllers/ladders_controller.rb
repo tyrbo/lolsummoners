54398319d949f3555916383bf4ed83f94ec86f75
Hashrocket Workstation <dev@hashrocket.com>
2014-02-25 22:32:45 -0800

    Downcase region

--- app/controllers/ladders_controller.rb
+++ app/controllers/ladders_controller.rb
@@ -1,6 +1,6 @@
 class LaddersController < ApplicationController
   def show
-    fail ActionController::RoutingError.new('Not Found') if !Region.available?(params[:region])
+    fail ActionController::RoutingError.new('Not Found') if !Region.available?(params[:region].downcase)
     @players = Ladder.new(params[:region]).find_by_page(params[:page].to_i)
   end
 end
