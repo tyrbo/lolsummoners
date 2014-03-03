2692141d808d88fab0fb4c9a0cfe597157735439
Hashrocket Workstation <dev@hashrocket.com>
2014-03-03 08:59:41 -0800

    Better routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Lolsummoners::Application.routes.draw do
   root 'posts#index'
   resources :posts, only: [:index, :show]
-  resources :ladders, only: [:show]
-#  get 'ladders(/:region(/:page))', to: 'ladders#show',
-#                             defaults: { region: 'all', page: 1 },
-#                                   as: 'ladders',
-#                          constraints: { page: /\d/ }
+
+  get 'ladders/:id/(:page)', to: 'ladders#show',
+                       defaults: { id: 'all', page: 1 },
+                             as: 'ladder',
+                    constraints: { page: /\d/ }
+
+  get 'ladders', to: 'ladders#show',
+           defaults: { id: 'all', page: 1 }
 end
