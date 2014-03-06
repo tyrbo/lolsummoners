cb4ce2d951380f834c4620b51b6293d1e10cf80a
Jonmichael Chambers <jon@tyrbo.net>
2014-03-06 13:48:08 -0800

    Changing database_cleaner strategy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,6 +54,14 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
     DatabaseCleaner.start
   end
 
