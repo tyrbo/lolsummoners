19bdb194a2d35c8ce1297935ccc3d7374cb08073
Jonmichael Chambers <jon@tyrbo.net>
2014-03-09 16:36:42 -0700

    Added Coveralls

--- Gemfile
+++ Gemfile
@@ -31,4 +31,5 @@ group :test do
   gem 'database_cleaner'
   gem 'codeclimate-test-reporter'
   gem 'webmock'
+  gem 'coveralls', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 ENV["RAILS_ENV"] ||= 'test'
+require 'coveralls'
+Coveralls.wear!('rails')
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 require File.expand_path("../../config/environment", __FILE__)
