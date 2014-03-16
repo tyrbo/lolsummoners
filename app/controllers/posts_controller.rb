2bb0c17c3681f4871dc113169f64646ebb705066
Jonmichael Chambers <jon@tyrbo.net>
2014-03-15 22:38:24 -0700

    Fixed pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.paginate(page: params[:page])
+    @posts = Post.page(params[:page]).order('created_at DESC')
     @players = Ladder.new('all').find_by_page(1).first(10)
   end
 
