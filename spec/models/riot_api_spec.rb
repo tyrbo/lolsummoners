a690fb8bfd0dd13c4993ad015071e6208b8e093a
Jonmichael Chambers <jon@tyrbo.net>
2014-06-10 16:27:41 -0600

    Adds vcr fixtures to gitignore.

--- spec/models/riot_api_spec.rb
+++ spec/models/riot_api_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe RiotApi, vcr: true do
   describe '#by_name' do
-    it 'returns a single response for a valid player' do
+    it 'returns a hash with single entry for each valid player' do
       expect(RiotApi.new('na').by_name(['pentakill']).count).to eq 1
     end
 
@@ -12,11 +12,11 @@ describe RiotApi, vcr: true do
   end
 
   describe '#league_for' do
-    it 'returns a hash for a player in a league' do
+    it "returns a hash with single entry for a player's league" do
       expect(RiotApi.new('na').league_for([21848947]).count).to eq 1
     end
 
-    it 'returns nil for a player not in a league' do
+    it 'returns {} for a player not in a league' do
       expect(RiotApi.new('na').league_for([0])).to eq Hash.new
     end
   end
