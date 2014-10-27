708fb22852b6cb2f11f69c3ee91434370a279cb9
Jonmichael Chambers <jon@tyrbo.net>
2014-10-27 10:55:39 -0600

    LeagueBuilder now uses find_or_create_by. find_or_initialize_by was unnecessary.

--- app/models/league_builder.rb
+++ app/models/league_builder.rb
@@ -1,7 +1,6 @@
 class LeagueBuilder
   def self.create_or_update(name, tier, queue, region)
-    league = League.find_or_initialize_by(name: name, tier: tier, queue: queue, region: region)
-    league.save
+    league = League.find_or_create_by(name: name, tier: tier, queue: queue, region: region)
     league
   end
 end
