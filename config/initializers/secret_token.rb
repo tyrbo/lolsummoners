087810036bdf6aa666977fb95ee407bb5eceaae5
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-05-01 20:49:22 -0600

    Updated secret token env variable to work by default with C66

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,5 +12,5 @@
 Lolsummoners::Application.config.secret_key_base = if Rails.env.development? || Rails.env.test?
                                                      ('x' * 30)
                                                    else
-                                                     ENV['SECRET_KEY']
+                                                     ENV['SECRET_KEY_BASE']
                                                    end
