861500c88af516518919d4da1e8c3c0951236732
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-05-15 14:18:32 -0600

    Fixes PlayerLeagueBuilder to properly check that the PlayerLeague was changed (as opposed to the Player)

--- app/lib/player_league_builder.rb
+++ app/lib/player_league_builder.rb
@@ -4,11 +4,12 @@ class PlayerLeagueBuilder
   def self.create_or_update(player, attributes, region, league)
     attributes['league_id'] = league.id
     attributes = prepare_attributes(attributes)
+
     if player.player_league.nil?
       player.create_player_league(attributes)
     else
       player.player_league.assign_attributes(attributes)
-      if player.changed?
+      if player.player_league.changed?
         player.player_league.save
       end
     end
