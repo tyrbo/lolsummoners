2dd6c953d4482c65cdf4544f76878045d5cbeca9
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-05-21 13:50:59 -0600

    UpdaterJob can handle the region grouping still.

--- app/jobs/updater_job.rb
+++ app/jobs/updater_job.rb
@@ -3,10 +3,11 @@ class UpdaterJob < ActiveJob::Base
     #players = Player.find_players_to_update(1.day.ago).group_by(&:region)
     #break if players.length.zero?
 
-    ids.each do |region, batch|
+    players = Player.eager_load(:player_league).find(ids).group_by(&:region)
+
+    players.each do |region, batch|
       begin
-        players = Player.eager_load(:player_league).find(batch)
-        PlayerLeagueUpdater.update_all(region, players)
+        PlayerLeagueUpdater.update_all(region, batch)
       rescue RiotApi::InvalidStatusCode
         Rails.logger.warn("Received invalid status code on #{region} with #{batch.map(&:summoner_id).inspect}")
         next
