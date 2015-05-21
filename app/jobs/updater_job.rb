1ba58be08cd30765ebcecdc0d44bbb551f4761ee
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-05-21 13:43:11 -0600

    Sending across an array of ids rather than a shit ton of actual player objects.

--- app/jobs/updater_job.rb
+++ app/jobs/updater_job.rb
@@ -1,11 +1,12 @@
 class UpdaterJob < ActiveJob::Base
-  def perform(players)
+  def perform(ids)
     #players = Player.find_players_to_update(1.day.ago).group_by(&:region)
     #break if players.length.zero?
 
-    players.each do |region, batch|
+    ids.each do |region, batch|
       begin
-        PlayerLeagueUpdater.update_all(region, batch)
+        players = Player.eager_load(:player_league).find(batch)
+        PlayerLeagueUpdater.update_all(region, players)
       rescue RiotApi::InvalidStatusCode
         Rails.logger.warn("Received invalid status code on #{region} with #{batch.map(&:summoner_id).inspect}")
         next
