63dc35192da70e877483fb8d2b93650f7083606b
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-05-26 15:32:15 -0600

    Cleans up spec for League - spacing, single quotes vs double quotes, etc

--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -1,45 +1,55 @@
 require 'spec_helper'
 
 describe League do
-  describe '.add_points_for_tier' do
-    it 'should return 0 for BRONZE' do
-      expect(League.add_points_for_tier('BRONZE')).to eq 0
+  describe ".add_points_for_tier" do
+    it "should return 0 for BRONZE" do
+      expect(League.add_points_for_tier("BRONZE")).to eq 0
     end
-    it 'should return 5000 for SILVER' do
-      expect(League.add_points_for_tier('SILVER')).to eq 5000
+
+    it "should return 5000 for SILVER" do
+      expect(League.add_points_for_tier("SILVER")).to eq 5000
     end
-    it 'should return 10000 for GOLD' do
-      expect(League.add_points_for_tier('GOLD')).to eq 10000
+
+    it "should return 10000 for GOLD" do
+      expect(League.add_points_for_tier("GOLD")).to eq 10000
     end
-    it 'should return 15000 for PLATINUM' do
-      expect(League.add_points_for_tier('PLATINUM')).to eq 15000
+
+    it "should return 15000 for PLATINUM" do
+      expect(League.add_points_for_tier("PLATINUM")).to eq 15000
     end
-    it 'should return 20000 for DIAMOND' do
-      expect(League.add_points_for_tier('DIAMOND')).to eq 20000
+
+    it "should return 20000 for DIAMOND" do
+      expect(League.add_points_for_tier("DIAMOND")).to eq 20000
     end
-    it 'should return 25000 for MASTER' do
-      expect(League.add_points_for_tier('MASTER')).to eq 25000
+
+    it "should return 25000 for MASTER" do
+      expect(League.add_points_for_tier("MASTER")).to eq 25000
     end
-    it 'should return 30000 for CHALLENGER' do
-      expect(League.add_points_for_tier('CHALLENGER')).to eq 30000
+
+    it "should return 30000 for CHALLENGER" do
+      expect(League.add_points_for_tier("CHALLENGER")).to eq 30000
     end
   end
 
-  describe '.add_points_for_rank' do
-    it 'should return 4000 for I' do
-      expect(League.add_points_for_rank('I')).to eq 4000
+  describe ".add_points_for_rank" do
+    it "should return 4000 for I" do
+      expect(League.add_points_for_rank("I")).to eq 4000
     end
-    it 'should return 3000 for II' do
-      expect(League.add_points_for_rank('II')).to eq 3000
+
+    it "should return 3000 for II" do
+      expect(League.add_points_for_rank("II")).to eq 3000
     end
-    it 'should return 2000 for I' do
-      expect(League.add_points_for_rank('III')).to eq 2000
+
+    it "should return 2000 for III" do
+      expect(League.add_points_for_rank("III")).to eq 2000
     end
-    it 'should return 1000 for I' do
-      expect(League.add_points_for_rank('IV')).to eq 1000
+
+    it "should return 1000 for IV" do
+      expect(League.add_points_for_rank("IV")).to eq 1000
     end
-    it 'should return 0 for I' do
-      expect(League.add_points_for_rank('V')).to eq 0
+
+    it "should return 0 for V" do
+      expect(League.add_points_for_rank("V")).to eq 0
     end
   end
 
