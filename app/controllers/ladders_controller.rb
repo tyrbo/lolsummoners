73d77acb67f1d2314d7b8f6bf49f84e4733f1256
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-06-08 03:18:41 -0600

    Ladder uses a decorator

--- app/controllers/ladders_controller.rb
+++ app/controllers/ladders_controller.rb
@@ -2,11 +2,15 @@ class LaddersController < ApplicationController
   before_action :prepare_params
 
   def show
-    @players = Ladder.new(params[:region]).find_by_page(params[:page])
+    @players = PlayerDecorator.decorate_collection(players_for_page)
     @total = Redis.current.get("total_#{params[:region]}").to_f
   end
 
   private
+
+  def players_for_page
+    Ladder.new(params[:region]).find_by_page(params[:page])
+  end
 
   def prepare_params
     params[:page] = params[:page].to_i
