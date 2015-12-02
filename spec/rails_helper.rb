e87edc45475a7f92de8ed556fc594408bc29a0dd
Jonmichael Chambers <jon@tyrbo.net>
2015-12-01 20:58:07 -0700

    Added gems and more setup things

---
+++ spec/rails_helper.rb
@@ -0,0 +1,23 @@
+ENV['RAILS_ENV'] ||= "test"
+
+require File.expand_path("../../config/environment", __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require "spec_helper"
+require "rspec/rails"
+require "capybara/rspec"
+
+Capybara.javascript_driver = :webkit
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.filter_rails_from_backtrace!
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+
+  config.infer_spec_type_from_file_location!
+end
