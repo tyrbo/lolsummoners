e983641bd7b61919be1abea23dea36efd8838515
Jonmichael Chambers <jon@tyrbo.net>
2015-12-29 03:19:07 -0700

    Add phoenix_haml, add dockerignore

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule App.Mixfile do
      {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:phoenix_haml, github: "chrismccord/phoenix_haml"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
