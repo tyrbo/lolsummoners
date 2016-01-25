2e3fbdd7e3843f9c17c450cd0fb52a1830712f08
Jonmichael Chambers <jon@tyrbo.net>
2016-01-24 21:43:23 -0700

    Fresh go at a cleaner look (maybe)

--- web/controllers/ladder_controller.ex
+++ web/controllers/ladder_controller.ex
@@ -2,6 +2,15 @@ defmodule App.LadderController do
   use App.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> assign(:leagues, "9.7K")
+    |> assign(:new, "23.7K")
+    |> assign(:players, players)
+    |> assign(:total, "11.4M")
+    |> assign(:updates, "44.3K")
+    |> render("index.html")
+  end
+
+  defp players do
   end
 end
