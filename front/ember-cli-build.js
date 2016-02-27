0f89404c99eb1d08f4e8b853a760774572c36b5b
Jonmichael Chambers <jon@tyrbo.net>
2016-02-26 18:29:15 -0700

    Adds style to ember

--- front/ember-cli-build.js
+++ front/ember-cli-build.js
@@ -7,10 +7,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  app.import('vendor/app.js');
-  app.import('vendor/ladder.js');
-  app.import('vendor/socket.js');
   app.import('vendor/toolkit.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
